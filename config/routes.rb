41ca27a9c572c161f6a8aeb4eead6c87990dafb4
Owen Peredo <owenperedo@gmail.com>
2016-09-26 09:26:40 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,2 @@
 Rails.application.routes.draw do
-
-    scope PluginRoutes.system_info["relative_url_root"] do
-      scope '(:locale)', locale: /#{PluginRoutes.all_locales}/, :defaults => {  } do
-        # frontend
-        namespace :plugins do
-          namespace 'cama_tinymce_template' do
-            get 'index' => 'front#index'
-          end
-        end
-      end
-
-      #Admin Panel
-      scope :admin, as: 'admin', path: PluginRoutes.system_info['admin_path_name'] do
-        namespace 'plugins' do
-          namespace 'cama_tinymce_template' do
-            get 'index' => 'admin#index'
-          end
-        end
-      end
-
-      # main routes
-      #scope 'cama_tinymce_template', module: 'plugins/cama_tinymce_template/', as: 'cama_tinymce_template' do
-      #  Here my routes for main routes
-      #end
-    end
-  end
+end
